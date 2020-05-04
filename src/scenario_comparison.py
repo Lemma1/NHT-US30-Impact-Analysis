c55eaaaf550c1b376bb5502dbdfcf9a0bbe940b2
Lemma1 <lemma171@gmail.com>
2020-05-03 22:49:01 -0400

    archive

---
+++ src/scenario_comparison.py
@@ -0,0 +1,17 @@
+import os
+import numpy as np
+
+
+class Scenario():
+  def __init__(self):
+    pass
+
+
+class ScenarioManger():
+  def __init__(self):
+    pass
+
+  def add_scenarios():
+    pass
+
+  
